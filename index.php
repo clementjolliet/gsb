b9fe363be0b485f99cdae51d838b1299929f27ab
Teri <teri.romain@gmail.com>
2015-04-09 14:16:38 +0200

    Ajout uc "comptable" fichier index.php

--- index.php
+++ index.php
@@ -19,6 +19,9 @@ switch($uc){
 	case 'etatFrais' :{
 		include("controleurs/c_etatFrais.php");break; 
 	}
+        case 'comptable' :{
+		include("controleurs/c_comptable.php");break; 
+	}
 }
 include("vues/v_pied.php") ;
 ?>
